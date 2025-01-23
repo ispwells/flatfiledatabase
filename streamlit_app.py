d7e234c1000800ca9bd8812839b593f56c8be94c
ispwells <ispwells@beechhallschool.org>
2025-01-23 10:20:01 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,66 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+class Record:
+    def __init__(self,id,forename,surname,age,email):
+        self.id = id
+        self.forename = forename
+        self.surname = surname
+        self.age = age
+        self.email = email
+    def to_csv(self):
+        return f"{self.id},{self.forename},{self.surname},{self.age},{self.email}"
+
+class FlatFileDataBase:
+    def __init__(self,filename):
+        self.filename = filename
+    def save_record(self,record):
+        with open(self.filename,"a") as file:
+            file.write(record.to_csv() + "\n")
+    def read_record(self):
+        with open(self.filename,"r") as file:
+            return [line.strip().split(",") for line in file]
+    def delete_record(self,record_id):
+        record = self.read_record()
+        records = [record for record in record if record[0] != str(record_id)]
+        with open(self.filename,"w") as file:
+            for record in records:
+                file.write (",".join(record)+"\n")
+
+
+db = FlatFileDataBase("database.txt")
+#record1 = Record(1,"Alice","Smith",30,"alice@example.com")
+#record2 = Record(2,"Bob","Brown",25,"bob@example.com")
+#record3 = Record(3,"Andrew","Kelly",16,"Akelly@example.com")
+#db.save_record(record1)
+#db.save_record(record2)
+#db.save_record(record3)
+def adding():
+
+
+    userid = int(input("please enter the user id"))
+    firstname = input("please enter the first name")
+    lastname = input("please enter the last name")
+    year = int(input("please enter the age of user"))
+    emailcontact = input("please enter the email")
+    record4 = Record(userid,firstname,lastname,year,emailcontact)
+    db.save_record(record4)
+add = input("would you like to add a user")
+if add == "yes":
+    adding()
+
+# Read records
+records = db.read_record()
+print("Records in the database:")
+for rec in records:
+    print(rec)
+
+# Delete a record
+delete = input("would you like to delete a user")
+if delete == "yes":
+    selcter = int(input("please enter a number to delete a user please use the ID"))
+    db.delete_record(selcter)
+    print("Updated records:")
+    records = db.read_record()
+    for rec in records:
+        print(rec)
